f4f039d5c028e29ba35b58a04821f4d07b867d44
YukiMiyatake <yukimariamiyatake@gmail.com>
2017-10-16 05:39:33 +0900

    MenuItem 1

---
+++ UNITYLesson/Assets/Menu/Editor/TestMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TestMenu
+{
+    [MenuItem("Test/Menu %a")]
+    public static void ShowMenu()
+    {
+        Debug.Log("ShowMenu");
+    }
+
+
+    [MenuItem("Test/AddObject %b", true)]
+    static bool AddObjectValidate()
+    {
+        // GameObjectか？
+        return Selection.activeObject.GetType() == typeof(GameObject);
+    }
+
+    [MenuItem("Test/AddObject %b", false)]
+    static void AddObject(MenuCommand command)
+    {
+        GameObject objMenuCommand = new GameObject("MenuCommand");
+        //　Undo登録
+        Undo.RegisterCreatedObjectUndo(objMenuCommand, "Test_AddObject " + objMenuCommand.name);
+        GameObjectUtility.SetParentAndAlign(objMenuCommand, command.context as GameObject);
+
+
+        GameObject obj = new GameObject("NewGame");
+        GameObjectUtility.SetParentAndAlign(obj, Selection.activeGameObject );
+
+        //　Undo登録
+        Undo.RegisterCreatedObjectUndo(obj, "Test_AddObject " + obj.name);
+        Undo.AddComponent<Rigidbody>(obj);
+
+    }
+
+}
