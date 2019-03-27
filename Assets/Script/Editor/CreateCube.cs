43097ceb62f7408685ecb7d74526a7f6f24a4a68
YukiMiyatake <yukimariamiyatake@gmail.com>
2019-03-28 04:09:59 +0900

    細かい修正

--- Assets/Script/Editor/CreateCube.cs
+++ Assets/Script/Editor/CreateCube.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Runtime.CompilerServices;
-using NUnit.Framework;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEditor;
-using System.Collections.Generic;
-using System.Xml.Schema;
-using Random = System.Random;
 
 namespace Openworld.Editor
 {
@@ -15,24 +9,31 @@ namespace Openworld.Editor
 
         public static Rect size = new Rect(-1000, 0, 2000, 1000);
 
-        [MenuItem("Split/CreateCube")]
+        [MenuItem("OpenWorld/CreateCube")]
         public static void Init()
         {
             GetWindow<CreateCube>();
-
-            ptable[0] = new GameObject();
-            ptable[0].name = "F_";
-            ptable[1] = new GameObject();
-            ptable[1].name = "M_";
-            ptable[2] = new GameObject();
-            ptable[2].name = "N_";
         }
 
 
         public void OnGUI()
         {
+            // if folder already exist
+            if (GameObject.Find("/F_") || GameObject.Find("/M_") || GameObject.Find("/N_"))
+            {
+                GUILayout.Label("Already exists!!");
+                return;
+            }
+
             if (GUILayout.Button("Create!"))
             {
+                ptable[0] = new GameObject();
+                ptable[0].name = "F_";
+                ptable[1] = new GameObject();
+                ptable[1].name = "M_";
+                ptable[2] = new GameObject();
+                ptable[2].name = "N_";
+
                 for (var z = size.y; z < size.y+size.height; z+=50)
                 {
                     for (var x = size.x; x < size.x+size.width; x+=50)
